2713a1065525ddfc332bb2c5fe6e8e610aa70ca7
Shanmukh Magapu <89508567+SHANMUKH405@users.noreply.github.com>
2022-06-22 18:17:16 +0530

    Add files via upload
    Contact me at: LinkedIn- https://www.linkedin.com/in/shanmukh-magapu-a6639520b
    
    GitHub: https://github.com/SHANMUKH405
    
    Resume : 
    https://drive.google.com/file/d/1OEIG9tsgvlSJHDMHNcuLe8h9LaykwAaC/view?usp=drivesdk
    My portfolio: https://shanmukh405.github.io/My-Resume/
    
    React Projects: https://www.evernote.com/shard/s654/sh/9b69596c-e3fd-d36f-8843-ec06de26db87/ba4dd72b3a853e0f83008bd736382fa5

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'xxx',
+    authDomain: 'angular-10-crud-db.firebaseapp.com',
+    databaseURL: 'https://angular-10-crud-db.firebaseio.com',
+    projectId: 'angular-10-crud-db',
+    storageBucket: 'angular-10-crud-db.appspot.com',
+    messagingSenderId: 'xxx',
+    appId: '1:xxx:web:xxx'
+  }
+};
